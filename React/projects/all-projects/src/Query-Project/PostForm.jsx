5c7a929d32b5b3cf25934f549d678a3e1ccd835d
rohitcodism <rohit.paul.22@aot.edu.in>
2023-09-14 07:46:10 +0530

    do it

--- React/projects/all-projects/src/Query-Project/PostForm.jsx
+++ React/projects/all-projects/src/Query-Project/PostForm.jsx
@@ -1,18 +1,64 @@
-export const PostForm = () => {
+import { useState } from "react";
 
-    const Field = (label) =>{
-        <div>
-            <label>{label}</label>
-            <input  type="text" name={label.toLowerCase()} />
-        </div>
+/**
+ * Renders a form with input fields for "Title" and "Content".
+ * @returns {JSX.Element} The rendered form component.
+ */
+export const PostForm = () => {
+    const [post, setPost] = useState({
+        title: '',
+        content: '',
+    });
+
+    /**
+     * Handles changes in the input fields.
+     * @param {Event} event - The input change event.
+     */
+    const handleChangeInput = (event) => {
+        const { name, value } = event.target;
+        setPost((prevPost) => ({
+            ...prevPost,
+            [name]: value,
+        }));
     };
 
-    return(
-        <>
-            <form>
-                {Field("Title")}
-                {Field("Content")}
+    /**
+     * Handles form submission.
+     * @param {Event} event - The form submit event.
+     */
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log(post);
+        setPost({
+            title: '',
+            content: '',
+        });
+    };
+
+    /**
+     * Generates a div element containing a label and an input field.
+     * @param {string} label - The label for the input field.
+     * @returns {JSX.Element} The generated div element.
+     */
+    const Field = ({ label }) => (
+        <div>
+            <label htmlFor={label.toLowerCase()}>{label}</label>
+            <input
+                onChange={handleChangeInput}
+                type="text"
+                name={label.toLowerCase()}
+                value={post[label.toLowerCase()]}
+            />
+        </div>
+    );
+
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <Field label="Title" />
+                <Field label="Content" />
+                <button type="submit">Create</button>
             </form>
-        </>
+        </div>
     );
 };
