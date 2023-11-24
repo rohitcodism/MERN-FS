42db1800776c0f0646c9ab89715850b422ed13bb
rohitcodism <rohitpaula18i@gmail.com>
2023-11-24 07:26:40 +0530

    routing-wip

---
+++ Nextjs/tut-1/my-next-app/app/profile/page.tsx
@@ -0,0 +1,12 @@
+import { Container } from "@mui/material"
+
+export default function Profile()
+{
+    return(
+        <Container className="flex flex-col justify-center items-center mt-20">
+            <h1>Profile</h1>
+            <hr />
+            <p>Profile Page</p>
+        </Container>
+    );
+}
