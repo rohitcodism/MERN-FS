c6407132531b450279c4e4806921319dee899015
rohitcodism <rohitpaula18i@gmail.com>
2023-12-01 07:44:34 +0530

    next-wip-day-3

---
+++ Nextjs/bun-next/app/(consumer)/page.js
@@ -0,0 +1,43 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import { Suspense } from 'react'
+import Dashboard from '../home/dashboard/page.jsx'
+
+export default function Home() {
+    return (
+        <div
+            style={{
+                display: "flex",
+                justifyContent: 'center',
+                width: "100%"
+            }}
+        >
+            <h3>Main Page</h3>
+            <nav
+                style={{
+                    width: "50%",
+                }}
+            >
+                <ul
+                    style={{
+                        display: "flex",
+                        justifyContent: "space-evenly"
+                    }}
+                >
+                    <li>
+                        <Link href="/home">Home</Link>
+                    </li>
+                    <li>
+                        <Link href="/user">User</Link>
+                    </li>
+                    <li>
+                        <Link href="/about">About</Link>
+                    </li>
+                </ul>
+            </nav>
+            <Suspense fallback={null}>
+                <Dashboard />
+            </Suspense>
+        </div>
+    )
+}
