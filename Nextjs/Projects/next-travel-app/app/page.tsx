81f23edcfbdc395effd5b495c8ea6f2520ae7a66
rohitcodism <rohitpaula18i@gmail.com>
2023-12-08 22:31:22 +0530

    next-project-travel-app-started and other backend codes

---
+++ Nextjs/Projects/next-travel-app/app/page.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <div>
+      <h1
+        className="text-4xl font-bold text-center text-gray-900"
+      >
+        Travel App
+      </h1>
+    </div>
+  )
+}
