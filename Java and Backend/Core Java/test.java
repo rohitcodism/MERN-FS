d71c4b9bc19f830e71fdcb84c9cbbd91cd65a386
rohitcodism <rohitpaula18i@gmail.com>
2024-01-02 10:57:16 +0530

    Add user input functionality to test.java

--- Java and Backend/Core Java/test.java
+++ Java and Backend/Core Java/test.java
@@ -1,7 +1,13 @@
+import java.util.Scanner;
+
 class Test
 {
     public static void main(String[] args)
     {
-        System.out.println("Hello World!");
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.println("Enter a number: ");
+            int n = sc.nextInt();
+            System.out.println("You entered " + n);
+        }
     }
 }
